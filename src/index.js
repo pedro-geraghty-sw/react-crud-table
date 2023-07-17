b71c11b7994685328364ed1eeb7934bdb8f831b1
pedro-geraghty-sw <pedro.geraghty@southworks.com>
2023-07-17 13:11:03 -0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {useState} from 'react'
 import CRUDTable, {
   Fields,
   Field,
@@ -10,157 +11,196 @@ import CRUDTable, {
 
 // Component's Base CSS
 import "./index.css";
+import axios from "axios";
+
+
+
+const BASE_URL = "http://localhost/api/product";
 
 const DescriptionRenderer = ({ field }) => <textarea {...field} />;
 
-let tasks = [
-  {
-    id: 1,
-    title: "Create an example",
-    description: "Create an example of how to use the component"
-  },
-  {
-    id: 2,
-    title: "Improve",
-    description: "Improve the component!"
-  }
-];
+const Example = () => {
+  const [apiUrl, setApiUrl] = useState(BASE_URL);
+  
+  const HTTP_CLIENT = axios.create({
+    headers: { 'Content-Type': 'application/json' },
+    baseURL: apiUrl
+  });
 
-const SORTERS = {
-  NUMBER_ASCENDING: mapper => (a, b) => mapper(a) - mapper(b),
-  NUMBER_DESCENDING: mapper => (a, b) => mapper(b) - mapper(a),
-  STRING_ASCENDING: mapper => (a, b) => mapper(a).localeCompare(mapper(b)),
-  STRING_DESCENDING: mapper => (a, b) => mapper(b).localeCompare(mapper(a))
-};
+  let products = [];
+  HTTP_CLIENT.get().then((response) => {
+    products = response.data;
+  });
 
-const getSorter = data => {
-  const mapper = x => x[data.field];
-  let sorter = SORTERS.STRING_ASCENDING(mapper);
+  const SORTERS = {
+    NUMBER_ASCENDING: mapper => (a, b) => mapper(a) - mapper(b),
+    NUMBER_DESCENDING: mapper => (a, b) => mapper(b) - mapper(a),
+    STRING_ASCENDING: mapper => (a, b) => mapper(a).localeCompare(mapper(b)),
+    STRING_DESCENDING: mapper => (a, b) => mapper(b).localeCompare(mapper(a))
+  };
 
-  if (data.field === "id") {
-    sorter =
-      data.direction === "ascending"
-        ? SORTERS.NUMBER_ASCENDING(mapper)
-        : SORTERS.NUMBER_DESCENDING(mapper);
-  } else {
-    sorter =
-      data.direction === "ascending"
-        ? SORTERS.STRING_ASCENDING(mapper)
-        : SORTERS.STRING_DESCENDING(mapper);
-  }
+  const getSorter = data => {
+    const mapper = x => x[data.field];
+    let sorter = SORTERS.STRING_ASCENDING(mapper);
 
-  return sorter;
-};
+    if (data.field === "id") {
+      sorter =
+        data.direction === "ascending"
+          ? SORTERS.NUMBER_ASCENDING(mapper)
+          : SORTERS.NUMBER_DESCENDING(mapper);
+    } else {
+      sorter =
+        data.direction === "ascending"
+          ? SORTERS.STRING_ASCENDING(mapper)
+          : SORTERS.STRING_DESCENDING(mapper);
+    }
 
-let count = tasks.length;
-const service = {
-  fetchItems: payload => {
-    let result = Array.from(tasks);
-    result = result.sort(getSorter(payload.sort));
-    return Promise.resolve(result);
-  },
-  create: task => {
-    count += 1;
-    tasks.push({
-      ...task,
-      id: count
-    });
-    return Promise.resolve(task);
-  },
-  update: data => {
-    const task = tasks.find(t => t.id === data.id);
-    task.title = data.title;
-    task.description = data.description;
-    return Promise.resolve(task);
-  },
-  delete: data => {
-    const task = tasks.find(t => t.id === data.id);
-    tasks = tasks.filter(t => t.id !== task.id);
-    return Promise.resolve(task);
-  }
-};
+    return sorter;
+  };
 
-const styles = {
-  container: { margin: "auto", width: "fit-content" }
-};
+  let count = products.length;
+  const service = {
+    fetchItems: payload => {
+      let result = Array.from(products);
+      result = result.sort(getSorter(payload.sort));
+      console.log(result);
+      return Promise.resolve(result);
+    },
+    create: product => {
+      const newProduct = {
+        ...product
+      };
+      return HTTP_CLIENT.post(`${apiUrl}?name=${encodeURIComponent(newProduct.name)}`)
+        .then((response) => {
+          products.push(response.data);
+          return Promise.resolve(response.data);
+        })
+        .catch(error => {
+          // Handle error if needed
+          console.error('Error creating product:', error);
+          return Promise.reject(error);
+        });
+    },
+    update: data => {
+      const product = products.find(t => t.id === data.id);
+      product.name = data.name;
 
-const Example = () => (
-  <div style={styles.container}>
-    <CRUDTable
-      caption="Tasks"
-      fetchItems={payload => service.fetchItems(payload)}
-    >
-      <Fields>
-        <Field name="id" label="Id" hideInCreateForm />
-        <Field name="title" label="Title" placeholder="Title" />
-        <Field
-          name="description"
-          label="Description"
-          render={DescriptionRenderer}
+      return HTTP_CLIENT.put(`${apiUrl}/${product.id}?name=${encodeURIComponent(product.name)}`)
+        .then(() => {
+          return Promise.resolve(product);
+        })
+        .catch(error => {
+          // Handle error if needed
+          console.error('Error updating product:', error);
+          return Promise.reject(error);
+        });
+    },
+    delete: data => {
+      const product = products.find(t => t.id === data.id);
+      product.name = data.name;
+
+      return HTTP_CLIENT.delete(`${apiUrl}/${product.id}`)
+        .then(() => {
+          products = products.filter(t => t.id !== product.id);
+          return Promise.resolve();
+        })
+        .catch(error => {
+          // Handle error if needed
+          console.error('Error deleting product:', error);
+          return Promise.reject(error);
+        });
+    }
+  };
+
+  const styles = {
+    container: { margin: "auto", width: "fit-content" }
+  };
+
+  return (
+    <div style={styles.container}>
+      <input
+        type="text"
+        value={apiUrl}
+        onChange={(e) => setApiUrl(e.target.value)}
+      />
+
+      <CRUDTable
+        caption="products"
+        fetchItems={payload => service.fetchItems(payload)}
+        key="id"
+      >
+        <Fields>
+          <Field name="id" label="Id" hideInCreateForm />
+          <Field name="name" label="Name" placeholder="Name" />
+          {/* <Field
+            name="description"
+            label="Description"
+            render={DescriptionRenderer}
+          /> */}
+        </Fields>
+        <CreateForm
+          name="Product Creation"
+          message="Create a new product!"
+          trigger="Create product"
+          onSubmit={product => service.create(product)}
+          submitText="Create"
+          validate={values => {
+            const errors = {};
+            if (!values.name) {
+              errors.name = "Please, provide product's name";
+            }
+
+            // if (!values.description) {
+            //   errors.description = "Please, provide product's description";
+            // }
+
+            return errors;
+          }}
         />
-      </Fields>
-      <CreateForm
-        title="Task Creation"
-        message="Create a new task!"
-        trigger="Create Task"
-        onSubmit={task => service.create(task)}
-        submitText="Create"
-        validate={values => {
-          const errors = {};
-          if (!values.title) {
-            errors.title = "Please, provide task's title";
-          }
 
-          if (!values.description) {
-            errors.description = "Please, provide task's description";
-          }
+        <UpdateForm
+          name="product Update Process"
+          message="Update product"
+          trigger="Update"
+          onSubmit={product => service.update(product)}
+          submitText="Update"
+          validate={values => {
+            const errors = {};
 
-          return errors;
-        }}
-      />
+            if (!values.id) {
+              errors.id = "Please, provide id";
+            }
 
-      <UpdateForm
-        title="Task Update Process"
-        message="Update task"
-        trigger="Update"
-        onSubmit={task => service.update(task)}
-        submitText="Update"
-        validate={values => {
-          const errors = {};
+            if (!values.name) {
+              errors.name = "Please, provide product's name";
+            }
 
-          if (!values.id) {
-            errors.id = "Please, provide id";
-          }
+            // if (!values.description) {
+            //   errors.description = "Please, provide product's description";
+            // }
 
-          if (!values.title) {
-            errors.title = "Please, provide task's title";
-          }
+            return errors;
+          }}
+        />
 
-          if (!values.description) {
-            errors.description = "Please, provide task's description";
-          }
-
-          return errors;
-        }}
-      />
-
-      <DeleteForm
-        title="Task Delete Process"
-        message="Are you sure you want to delete the task?"
-        trigger="Delete"
-        onSubmit={task => service.delete(task)}
-        submitText="Delete"
-        validate={values => {
-          const errors = {};
-          if (!values.id) {
-            errors.id = "Please, provide id";
-          }
-          return errors;
-        }}
-      />
-    </CRUDTable>
-  </div>
-);
+        <DeleteForm
+          name="product Delete Process"
+          message="Are you sure you want to delete the product?"
+          trigger="Delete"
+          onSubmit={product => service.delete(product)}
+          submitText="Delete"
+          validate={values => {
+            const errors = {};
+            if (!values.id) {
+              errors.id = "Please, provide id";
+            }
+            return errors;
+          }}
+        />
+      </CRUDTable>
+    </div>
+  );
+};
 
 Example.propTypes = {};
 
